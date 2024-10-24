b30c0013a39be7631fdd9f989e9a3bd4710a92c7
aditya <avats218@gmail.com>
2024-10-24 10:52:36 +0530

    inventory Integration Test

---
+++ inventory-service/src/test/java/vatsify/io/inventory/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package vatsify.io.inventory;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
